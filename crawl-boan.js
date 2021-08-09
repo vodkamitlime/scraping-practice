6b3660ab3000daadfc4de0d27bf5a5e446525d21
Haeun Kang <tejavascript@gmail.com>
2021-08-09 23:36:56 +0900

    Complete data dxtraction from boannews

--- crawl-boan.js
+++ crawl-boan.js
@@ -3,12 +3,6 @@ const cheerio = require('cheerio');
 const iconv = require('iconv-lite');
 iconv.skipDecodeWarning = true
 
-// "article_title": < String > ,
-// "article_content": < String > ,
-//     "article_date": < Date > ,
-// "article_keyword": < String > ,
-//     "article_url": < String > ,
-
 const getSecurity = async () => {
 
     let articles = [];
@@ -26,10 +20,16 @@ const getSecurity = async () => {
         const title = $(article).find('.news_txt').text();
         const content = $(article).find('.news_content').text();
         const date = $(article).find('.news_writer').text();
-        console.log(url, title, content, date);
+        let DATA = {
+            "article_title": title,
+            "article_content": content,
+            "article_date": date.split('| ')[1],
+            "article_url": 'https://www.boannews.com/' + url, 
+            "article_keyword": "보안"
+        }
+        articles.push(DATA); 
     }
-
     return articles;
 }
 
-getSecurity();
+getSecurity().then(data => console.log(data))
